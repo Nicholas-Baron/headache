a1108ed87582feb78bce7b433ef65d75cbb00ab8
Nicholas Baron <nicholas.baron.ten@gmail.com>
2019-11-25 13:28:13 -0800

    Formatting pass

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-
+use std::collections::{hash_map, HashMap};
 use std::fs;
-use std::collections::{HashMap, hash_map};
 use std::path::PathBuf;
 use structopt::StructOpt;
 
@@ -8,43 +7,40 @@ use structopt::StructOpt;
 /// Complies to http://www.muppetlabs.com/~breadbox/bf/standards.html
 
 #[derive(StructOpt)]
-#[structopt(name="headache", about="An interpreter for Brainfuck")]
-struct CliOpt{
-
+#[structopt(name = "headache", about = "An interpreter for Brainfuck")]
+struct CliOpt {
     /// Enables debug outputs
-    #[structopt(short,long)]
+    #[structopt(short, long)]
     debug: bool,
 
     /// Input filepath
     input: PathBuf,
 }
 
-fn load_program(filename : &str) -> Option<String>{
-
-    fn is_bf_command(c : char) -> bool{
-        c == '.' || c == ',' || c == '[' || c == ']'
-            || c == '<' || c == '>' || c == '+' || c == '-'
+fn load_program(filename: &str) -> Option<String> {
+    fn is_bf_command(c: char) -> bool {
+        c == '.' || c == ',' || c == '[' || c == ']' || c == '<' || c == '>' || c == '+' || c == '-'
     }
 
-    match fs::read_to_string(filename){
-        Err(_) => { println!("Could not read file"); None },
-        Ok(data) => Some(data.chars().filter(|&x| is_bf_command(x)).collect())
+    match fs::read_to_string(filename) {
+        Err(_) => {
+            println!("Could not read file");
+            None
+        }
+        Ok(data) => Some(data.chars().filter(|&x| is_bf_command(x)).collect()),
     }
 }
 
-fn run_program (commands : String) -> (i32, HashMap::<i32, u8>) {
-    
+fn run_program(commands: String) -> (i32, HashMap<i32, u8>) {
     let mut ptr = 0;
     let mut ram = HashMap::new();
     for command in commands.chars() {
         match command {
-            '.' => {
-                match  ram.entry(ptr){
-                    hash_map::Entry::Occupied(entry) => print!("{}", entry.get()),
-                    _ => {}
-                }
+            '.' => match ram.entry(ptr) {
+                hash_map::Entry::Occupied(entry) => print!("{}", entry.get()),
+                _ => {}
             },
-            ',' => {},
+            ',' => {}
             '<' => ptr -= 1,
             '>' => ptr += 1,
             _ => panic!("Unimplemented BF command: {}", command),
@@ -54,12 +50,17 @@ fn run_program (commands : String) -> (i32, HashMap::<i32, u8>) {
     (ptr, ram)
 }
 
-fn main(){
-
+fn main() {
     let opt = CliOpt::from_args();
-    
+
     // First, read the program
-    let program = load_program(opt.input.into_os_string().to_str().expect("Could not read input file path")).expect("Could not read program");
+    let program = load_program(
+        opt.input
+            .into_os_string()
+            .to_str()
+            .expect("Could not read input file path"),
+    )
+    .expect("Could not read program");
 
     // Then, execute it in some machine
     let final_state = run_program(program);
@@ -68,12 +69,12 @@ fn main(){
         let width = 10;
         let center = final_state.0;
 
-        for i in center - (width/2).. center + (width/2){
+        for i in center - (width / 2)..center + (width / 2) {
             print!("{:3}", i);
         }
         println!("");
 
-        for i in center - (width/2).. center + (width/2){
+        for i in center - (width / 2)..center + (width / 2) {
             print!("{:3}", final_state.1.get(&i).unwrap_or(&0));
         }
         println!("");
