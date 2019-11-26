892ed504011d155274896cac3d8265433a2113bc
Nicholas Baron <nicholas.baron.ten@gmail.com>
2019-11-26 08:37:07 -0800

    Implemented user input

--- src/turing_machine.rs
+++ src/turing_machine.rs
@@ -56,6 +56,7 @@ pub struct TuringMachine {
     ram: HashMap<i32, u32>,
     pc: usize,
     program: Vec<Command>,
+    user_input: String,
 }
 
 impl TuringMachine {
@@ -65,7 +66,28 @@ impl TuringMachine {
             pc: 0,
             ram: HashMap::new(),
             program,
+            user_input: String::new(),
         }
+    }
+
+    fn read_user_input(&mut self) -> char {
+        if self.user_input.is_empty() {
+            use std::io::{stdin, stdout, Write};
+            let _ = stdout().flush();
+            stdin()
+                .read_line(&mut self.user_input)
+                .expect("Could not read user input.");
+
+            if let Some('\n') = self.user_input.chars().next_back() {
+                self.user_input.pop();
+            }
+
+            if let Some('\r') = self.user_input.chars().next_back() {
+                self.user_input.pop();
+            }
+        }
+
+        self.user_input.drain(..1).next().unwrap()
     }
 
     fn read_ram_ptr(&self) -> u32 {
@@ -84,7 +106,10 @@ impl TuringMachine {
             AddOne => self.write_ram_ptr(self.read_ram_ptr() + 1),
             SubOne => self.write_ram_ptr(self.read_ram_ptr() - 1),
             Output => print!("{}", std::char::from_u32(self.read_ram_ptr()).unwrap()),
-            Input => unimplemented!(),
+            Input => {
+                let value = self.read_user_input() as u32;
+                self.write_ram_ptr(value);
+            }
             JumpForward => {
                 if self.read_ram_ptr() == 0 {
                     let mut depth = 0;
